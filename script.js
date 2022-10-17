71210724f6439c262a6c6f66f5e20dc06cd6965d
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2022-10-16 20:00:45 -0400

    Completed length validation and choose selction validation

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function generatePassword(){
+  var passwordLen = parseInt(prompt("Enter password length"))
+  // "Name" 122
+   //"12"
+   if(passwordLen < 8 || passwordLen > 128 || isNaN(passwordLen)){
+    return  "Please enter valid length between 8-128"
+   }
+   var acceptU = confirm("Uppercase?")
+   var acceptN = confirm("Numeric?")
+   var acceptS = confirm("Special characters?")
+   var acceptT = confirm("lower case?")
+   if(!acceptU && !acceptN && !acceptS && !acceptT){
+   return "Please select at least one option"
+   }
+  
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
